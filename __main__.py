03dd1661bc88a2ed3298d7d682369a9e54571b7f
John Dutchover <johndutchover@users.noreply.github.com>
2024-02-25 00:09:11 -0500

    GKE cluster up

--- __main__.py
+++ __main__.py
@@ -1,75 +1,123 @@
 import pulumi
-import pulumi_gcp as gcp
-
-# Configure the GCP project and zone
-project_id = "premium-botany-414502"
-zone = "us-east4-c"
-
-# Define pub ssh key
-ssh_key = (
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMLbPkjTQ6CTLLzwNBXy6qfjLx9Xh+Lq004Ko7bS++Yn"
+from pulumi import Config, export, get_project, get_stack, Output, ResourceOptions
+from pulumi_gcp.config import project, zone
+from pulumi_gcp.container import Cluster, ClusterNodeConfigArgs
+from pulumi_kubernetes import Provider
+from pulumi_kubernetes.apps.v1 import Deployment, DeploymentSpecArgs
+from pulumi_kubernetes.core.v1 import (
+    ContainerArgs,
+    PodSpecArgs,
+    PodTemplateSpecArgs,
+    Service,
+    ServicePortArgs,
+    ServiceSpecArgs,
 )
+from pulumi_kubernetes.meta.v1 import LabelSelectorArgs, ObjectMetaArgs
 
-# Protect the VPC network from accidental deletion
-network = gcp.compute.Network("protected-network",
-                                        auto_create_subnetworks=True,
-                                        project=project_id,
-                                        opts=pulumi.ResourceOptions(protect=False))
+# Import the configuration values
+config = pulumi.Config()
 
-# Create a GCP firewall rule that allows SSH traffic from a given IP range
-firewall = gcp.compute.Firewall(
-    "firewall",
-    network=network.id,
-    allows=[
-        {
-            "protocol": "tcp",
-            "ports": ["22"],
-        }
-    ],
-    source_ranges=["71.247.198.14/32", "35.235.240.0/20"],
-)
+# Retrieve the value of "myEnvironment"
+myValue = config.require("myEnvironment")
 
-# Create a new GCP compute instance
-instance = gcp.compute.Instance(
-    "instance",
-    machine_type="e2-medium",
-    zone=zone,
-    boot_disk=gcp.compute.InstanceBootDiskArgs(
-        initialize_params=gcp.compute.InstanceBootDiskInitializeParamsArgs(
-            image="ubuntu-os-cloud/ubuntu-minimal-2204-lts"
-        )
+# Export the value as an output
+pulumi.export("environment", myValue)
+
+# nodeCount is the number of cluster nodes to provision. Defaults to 3 if unspecified.
+NODE_COUNT = config.get_int("node_count") or 3
+# nodeMachineType is the machine type to use for cluster nodes. Defaults to n1-standard-1 if unspecified.
+# See https://cloud.google.com/compute/docs/machine-types for more details on available machine types.
+NODE_MACHINE_TYPE = config.get("node_machine_type") or "e2-medium"
+# username is the admin username for the cluster.
+USERNAME = config.get("username") or "admin"
+# password is the password for the admin user in the cluster.
+PASSWORD = config.get_secret('clusterAdminPwd')
+# master version of GKE engine
+MASTER_VERSION = config.get('master_version')
+
+# Now, actually create the GKE cluster.
+k8s_cluster = Cluster(
+    "gke-cluster",
+    initial_node_count=NODE_COUNT,
+    node_version=MASTER_VERSION,
+    min_master_version=MASTER_VERSION,
+    node_config=ClusterNodeConfigArgs(
+        machine_type=NODE_MACHINE_TYPE,
+        oauth_scopes=[
+            "https://www.googleapis.com/auth/compute",
+            "https://www.googleapis.com/auth/devstorage.read_only",
+            "https://www.googleapis.com/auth/logging.write",
+            "https://www.googleapis.com/auth/monitoring",
+        ],
     ),
-    # Setting the scheduling option to preemptible for cost savings
-    scheduling=gcp.compute.InstanceSchedulingArgs(
-        preemptible=True,
-        automatic_restart=False
-    ),
-    network_interfaces=[
-        gcp.compute.InstanceNetworkInterfaceArgs(
-            network=network.id,
-            access_configs=[gcp.compute.InstanceNetworkInterfaceAccessConfigArgs()],
-        )
-    ],
-    service_account=gcp.compute.InstanceServiceAccountArgs(
-        scopes=["https://www.googleapis.com/auth/cloud-platform"]
-    ),
-    opts=pulumi.ResourceOptions(protect=False)
 )
 
-# A key set in project metadata is propagated to every instance in the project.
-# This resource configuration is prone to causing frequent diffs
-# as Google adds SSH Keys when the SSH Button is pressed in the console.
-# It is better to use OS Login instead.
-my_ssh_key = gcp.compute.ProjectMetadata(
-    "mySshKey",
-    metadata={
-        "ssh-keys": """john:ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMLbPkjTQ6CTLLzwNBXy6qfjLx9Xh+Lq004Ko7bS++Yn""",
-    },
+# Manufacture a GKE-style Kubeconfig. Note that this is slightly "different" because of the way GKE requires
+# gcloud to be in the picture for cluster authentication (rather than using the client cert/key directly).
+k8s_info = Output.all(k8s_cluster.name, k8s_cluster.endpoint, k8s_cluster.master_auth)
+k8s_config = k8s_info.apply(
+    lambda info: """apiVersion: v1
+clusters:
+- cluster:
+    certificate-authority-data: {0}
+    server: https://{1}
+  name: {2}
+contexts:
+- context:
+    cluster: {2}
+    user: {2}
+  name: {2}
+current-context: {2}
+kind: Config
+preferences: {{}}
+users:
+- name: {2}
+  user:
+    exec:
+      apiVersion: client.authentication.k8s.io/v1beta1
+      command: gke-gcloud-auth-plugin
+      installHint: Install gke-gcloud-auth-plugin for use with kubectl by following
+        https://cloud.google.com/blog/products/containers-kubernetes/kubectl-auth-changes-in-gke
+      provideClusterInfo: true
+""".format(
+        info[2]["cluster_ca_certificate"],
+        info[1],
+        "{0}_{1}_{2}".format(project, zone, info[0]),
+    )
 )
 
-# Export the external IP of the compute instance
-pulumi.export(
-    "instance_external_ip", instance.network_interfaces[0].access_configs[0].nat_ip
+# Make a Kubernetes provider instance that uses our cluster from above.
+k8s_provider = Provider("gke_k8s", kubeconfig=k8s_config)
+
+# Create a canary deployment to test that this cluster works.
+labels = {"app": "canary-{0}-{1}".format(get_project(), get_stack())}
+canary = Deployment(
+    "canary",
+    spec=DeploymentSpecArgs(
+        selector=LabelSelectorArgs(match_labels=labels),
+        replicas=1,
+        template=PodTemplateSpecArgs(
+            metadata=ObjectMetaArgs(labels=labels),
+            spec=PodSpecArgs(containers=[ContainerArgs(name="nginx", image="nginx")]),
+        ),
+    ),
+    opts=ResourceOptions(provider=k8s_provider),
 )
-pulumi.export('protected_network', network.name)
-pulumi.export('network_id', network.id)
+
+ingress = Service(
+    "ingress",
+    spec=ServiceSpecArgs(
+        type="LoadBalancer",
+        selector=labels,
+        ports=[ServicePortArgs(port=80)],
+    ),
+    opts=ResourceOptions(provider=k8s_provider),
+)
+
+# Finally, export the kubeconfig so that the client can easily access the cluster.
+export("kubeconfig", k8s_config)
+# Export the k8s ingress IP to access the canary deployment
+export(
+    "ingress_ip",
+    ingress.status.apply(lambda status: status.load_balancer.ingress[0].ip),
+)
